94b5428186fb04a2c533726184f7504f3329fef1
Uchechi Nmecha <63749773+UchechiNmecha@users.noreply.github.com>
2026-02-01 12:08:24 +0100

    Pointers Completed

---
+++ intermediate/pointers.go
@@ -0,0 +1,21 @@
+package basics
+
+import "fmt"
+
+func main() {
+	var ptr *int
+	var a int = 10
+	ptr = &a
+
+	fmt.Println(a)
+	fmt.Println(ptr)
+	fmt.Println(*ptr)
+
+	modifyValue(ptr)
+
+	fmt.Println(a)
+}
+
+func modifyValue(ptr *int) {
+	*ptr++
+}
